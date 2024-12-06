5abed2bb710c9c8b6d0a1a53dfc7ddb735518af0
Nicolas15-a <nicolasaliasgato@gmail.com>
2024-12-06 18:36:13 -0500

    change title and svg

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -114,5 +114,6 @@ export default function VocabularyGenerator() {
         </div>
       </CardContent>
     </Card>
+    
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+// base: '/Vocabulary-Generator/', // Solo el nombre del repositorio con barras inclinadas
 export default defineConfig({
-  base: '/Vocabulary-Generator/', // Solo el nombre del repositorio con barras inclinadas
+  base: './', // Configuración para rutas relativas
   plugins: [react()],
 })
